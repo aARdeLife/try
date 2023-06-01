e36688f3f044d1929ad9ee5a5b3ed3401348ea1a
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-06-01 22:43:05 +0200

    Update Keyboard.js

--- Keyboard.js
+++ Keyboard.js
@@ -1,76 +1,40 @@
-// Function to start the video stream from the front-facing camera
-async function startVideo() {
+// Function to start eye tracking
+async function startEyeTracking() {
   const videoElement = document.getElementById('video');
   
   try {
     const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
     videoElement.srcObject = stream;
+    
+    const gazeTracker = new xgaze.GazeTracker();
+    await gazeTracker.load();
+    gazeTracker.start(videoElement);
+    
+    // Listen for gaze updates
+    gazeTracker.onGazeUpdate((gazeData) => {
+      highlightKey(gazeData.x, gazeData.y);
+    });
   } catch (error) {
     console.error('Error accessing camera:', error);
   }
 }
 
-// Function to handle tapping on the screen and select the highlighted key
-function handleTap(event) {
-  const tappedElement = document.elementFromPoint(event.clientX, event.clientY);
-  
-  if (tappedElement.classList.contains('key')) {
-    const keyValue = tappedElement.dataset.key;
-  
-    if (keyValue === 'Space') {
-      textbox.value += ' ';
-    } else if (keyValue === 'Delete') {
-      textbox.value = textbox.value.slice(0, -1);
-    } else if (keyValue === 'Enter') {
-      textbox.value += '\n';
-    } else if (keyValue === 'Ctrl') {
-      // Handle Ctrl key functionality
-      // Add your code here
-    } else if (keyValue === 'Voice') {
-      // Handle Voice key functionality
-      // Add your code here
+// Function to highlight the key based on gaze position
+function highlightKey(x, y) {
+  const keys = document.querySelectorAll('.key');
+  keys.forEach((key) => {
+    const keyRect = key.getBoundingClientRect();
+    
+    if (x >= keyRect.left && x <= keyRect.right && y >= keyRect.top && y <= keyRect.bottom) {
+      key.classList.add('highlighted');
     } else {
-      textbox.value += keyValue;
+      key.classList.remove('highlighted');
     }
-  }
+  });
 }
 
 // Call the startVideo function when the page loads
 window.addEventListener('DOMContentLoaded', startVideo);
 
-// Add an event listener to the document to handle tapping on the screen
-document.addEventListener('click', handleTap);
-
-// Add the remaining code from your previous implementation here
-document.addEventListener('DOMContentLoaded', () => {
-  const keys = document.querySelectorAll('.key');
-  const textbox = document.getElementById('textInput');
-  const voiceButton = document.getElementById('voiceButton');
-
-  keys.forEach((key) => {
-    key.addEventListener('click', () => {
-      const keyValue = key.dataset.key;
-
-      if (keyValue === 'Space') {
-        textbox.value += ' ';
-      } else if (keyValue === 'Delete') {
-        textbox.value = textbox.value.slice(0, -1);
-      } else if (keyValue === 'Enter') {
-        textbox.value += '\n';
-      } else if (keyValue === 'Ctrl') {
-        // Handle Ctrl key functionality
-        // Add your code here
-      } else if (keyValue === 'Voice') {
-        // Handle Voice key functionality
-        // Add your code here
-      } else {
-        textbox.value += keyValue;
-      }
-    });
-  });
-
-  voiceButton.addEventListener('click', () => {
-    // Handle voice typing functionality here
-    // Add your code to start voice recognition
-  });
-});
+// Call the startEyeTracking function when the video stream is ready
+videoElement.addEventListener('loadeddata', startEyeTracking);
