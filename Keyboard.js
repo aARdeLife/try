1c7939890a8e3bd85af9dd681825f7abeeffc745
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-06-01 22:22:27 +0200

    Update Keyboard.js

--- Keyboard.js
+++ Keyboard.js
@@ -1,3 +1,47 @@
+// Function to start the video stream from the front-facing camera
+async function startVideo() {
+  const videoElement = document.getElementById('video');
+  
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user' } });
+    videoElement.srcObject = stream;
+  } catch (error) {
+    console.error('Error accessing camera:', error);
+  }
+}
+
+// Function to handle tapping on the screen and select the highlighted key
+function handleTap(event) {
+  const tappedElement = document.elementFromPoint(event.clientX, event.clientY);
+  
+  if (tappedElement.classList.contains('key')) {
+    const keyValue = tappedElement.dataset.key;
+  
+    if (keyValue === 'Space') {
+      textbox.value += ' ';
+    } else if (keyValue === 'Delete') {
+      textbox.value = textbox.value.slice(0, -1);
+    } else if (keyValue === 'Enter') {
+      textbox.value += '\n';
+    } else if (keyValue === 'Ctrl') {
+      // Handle Ctrl key functionality
+      // Add your code here
+    } else if (keyValue === 'Voice') {
+      // Handle Voice key functionality
+      // Add your code here
+    } else {
+      textbox.value += keyValue;
+    }
+  }
+}
+
+// Call the startVideo function when the page loads
+window.addEventListener('DOMContentLoaded', startVideo);
+
+// Add an event listener to the document to handle tapping on the screen
+document.addEventListener('click', handleTap);
+
+// Add the remaining code from your previous implementation here
 document.addEventListener('DOMContentLoaded', () => {
   const keys = document.querySelectorAll('.key');
   const textbox = document.getElementById('textInput');
